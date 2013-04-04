fab19ec51854c04b39f2331311016b8ce0564729
Sergiy Bondaryev <sbondaryev@gmail.com>
2013-04-04 18:01:26 +0200

    change data structure

--- example/data.js
+++ example/data.js
@@ -1,81 +1,92 @@
 var plannerData = [
     {
-        metadata: {
-            name: "Feature 1"
-        },
-        data: {
-            series: [ { start: "2013-01-03", end: "2013-01-05" }, { start: "2013-01-08", end: "2013-01-10" }],
-            series1: [ { start: "2013-01-07", end: "2013-01-09" }, ]
-        }
+        metadata: { name: "Feature 1" },
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [{ start: "2013-01-03", end: "2013-01-05" }, { start: "2013-01-08", end: "2013-01-10" }]
+            },
+            {
+                metadata: { name: "Sub 2" },
+                data: [ { start: "2013-01-07", end: "2013-01-09" }]
+            }            
+        ]
     }, 
     {
         metadata: {
             name: "Feature 2"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [{ start: "2013-01-03", end: "2013-01-05" }, { start: "2013-01-08", end: "2013-01-10" }]
+            }
+        ]
     }, 
     {
         metadata: {
             name: "Feature 3"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [ { start: "2013-01-07", end: "2013-01-09" }]
+            }            
+        ]
     }, 
     {
         metadata: {
             name: "Feature 4"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [ { start: "2013-01-07", end: "2013-01-09" }]
+            }            
+        ]
     },
     {
         metadata: {
             name: "Feature 5"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [ { start: "2013-01-07", end: "2013-01-09" }]
+            }            
+        ]
     }, 
     {
         metadata: {
             name: "Feature 6"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [ { start: "2013-01-07", end: "2013-01-09" }]
+            }            
+        ]
     }, 
     {
         metadata: {
             name: "Feature 7"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" , color: "#f0f0f0" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [ { start: "2013-01-07", end: "2013-01-09", color: "#f0f0f0" }]
+            }            
+        ]
     }, 
     {
         metadata: {
             name: "Feature 8"
         },
-        data: {
-            series: [
-                { start: "2013-01-03", end: "2013-01-05" , color: "#f0f0f0" },
-            ]
-        }
+        data: [
+            {
+                metadata: { name: "Sub 1" },
+                data: [ { start: "2013-01-07", end: "2013-01-09", color: "#f0f0f0" }]
+            }            
+        ]
     }
 ];
