274077a17831f932ec27f405c0d5a0a5d7d98d5d
Sergiy Bondaryev <sbondaryev@gmail.com>
2013-07-13 21:38:43 +0200

    centralize configuration whith using LazyLoading.js

---
+++ tests/pages/config.js
@@ -0,0 +1,12 @@
+var cssFiles = [
+    "../reset.css",
+    "../../../lib/jquery-ui-1.8.4.css",
+    "../../../jquery.scheduler.css"
+];
+
+var jsFiles = [
+    "../../../lib/jquery-1.5.1.min.js",
+    "../../../lib/jquery-ui-1.8.14.custom.min.js",
+    "../../../lib/date.js",
+    "../../../jquery.scheduler.js",
+];
